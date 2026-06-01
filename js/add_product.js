27dca503948d7b6d693c14f964aaf2496e153393
M. Zafid Affan <you@example.com>
2026-06-01 13:42:40 +0700

    fix deploy structure

--- js/add_product.js
+++ js/add_product.js
@@ -1,4 +1,4 @@
-const API_URL = "https://tugas-aplikasi-sederhana-ba9b6sark.vercel.app/api";
+const API_URL = "https://tugas-aplikasi-sederhana-ba9b6sark-zafid-affans-projects.vercel.app/api";
 
 async function saveProduct() {
     const nama = document.getElementById("nama").value;
