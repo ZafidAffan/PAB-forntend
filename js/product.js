e9e5e9c1efe7c44b83a3d567bd2d74b9999c6425
M. Zafid Affan <you@example.com>
2026-06-01 13:18:59 +0700

    fix deploy structure

--- js/product.js
+++ js/product.js
@@ -2,33 +2,48 @@ const API_URL = "https://tugas-aplikasi-sederhana-c80gfz3my-zafid-affans-project
 
 const token = localStorage.getItem("token");
 
+// redirect kalau belum login
 if (!token) {
     window.location.href = "login.html";
 }
 
 async function loadProducts() {
+    try {
+        const response = await fetch(`${API_URL}/products`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        });
 
-    const response = await fetch(`${API_URL}/products`, {
-        headers: {
-            Authorization: `Bearer ${token}`
+        const result = await response.json();
+
+        // DEBUG kalau error
+        if (!response.ok) {
+            console.log(result);
+            document.getElementById("productList").innerHTML =
+                `<li>${result.message}</li>`;
+            return;
         }
-    });
 
-    const res = await response.json();
+        const products = result.data;
 
-    const products = res.data;
+        let html = "";
 
-    let html = "";
+        products.forEach((product) => {
+            html += `
+                <li>
+                    ${product.nama} - Rp ${product.harga}
+                </li>
+            `;
+        });
 
-    products.forEach(product => {
-        html += `
-            <li>
-                ${product.nama} - Rp ${product.harga}
-            </li>
-        `;
-    });
+        document.getElementById("productList").innerHTML = html;
 
-    document.getElementById("productList").innerHTML = html;
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
 function logout() {
