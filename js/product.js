a32e782d7634b62e935e07d35552fa2344d35f1e
M. Zafid Affan <you@example.com>
2026-06-01 13:04:56 +0700

    fix deploy structure

--- js/product.js
+++ js/product.js
@@ -2,26 +2,28 @@ const API_URL = "https://tugas-aplikasi-sederhana-c80gfz3my-zafid-affans-project
 
 const token = localStorage.getItem("token");
 
-if(!token){
+if (!token) {
     window.location.href = "login.html";
 }
 
-async function loadProducts(){
+async function loadProducts() {
 
-    const response = await fetch(`${API_URL}/products`,{
-        headers:{
+    const response = await fetch(`${API_URL}/products`, {
+        headers: {
             Authorization: `Bearer ${token}`
         }
     });
 
-    const products = await response.json();
+    const res = await response.json();
+
+    const products = res.data;
 
     let html = "";
 
     products.forEach(product => {
         html += `
             <li>
-                ${product.name}
+                ${product.nama} - Rp ${product.harga}
             </li>
         `;
     });
@@ -29,7 +31,7 @@ async function loadProducts(){
     document.getElementById("productList").innerHTML = html;
 }
 
-function logout(){
+function logout() {
     localStorage.removeItem("token");
     window.location.href = "login.html";
 }
